82c690067602ae3873a8ce6e774bf3133c3eb339
Caleb Alonso <caleb_alonso24@hotmail.com>
2023-10-12 22:24:55 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const createCalendar = ({locale, year}) => {
+    const weekdays = [...Array(7).keys()]
+const intlWeekDay = new Intl.DateTimeFormat(locale, { weekday: 'long' })
+
+const weekDaysNames = weekdays.map(weekDayIndex => {
+    const date = new Date(2023, 4, weekDayIndex + 1)
+    const weekDayName = intlWeekDay.format(date)
+    return weekDayName
+})
+
+const renderedWeekDays = weekDaysNames.map
+(weekDayName => `<li class='day-name'>${weekDayName}</li>`).join('')
+
+const months = [...Array(12).keys()]
+const intl = new Intl.DateTimeFormat(locale, { month: 'long'})
+
+const calendar = months.map(monthKey => {
+    const monthName = intl.format(new Date(year, monthKey))
+
+    const nextMonthIndex = monthKey + 1
+    const daysOfMonth =  new Date(year, nextMonthIndex, 0).getDate()
+
+    const startsOn = new Date(year, monthKey, 1).getDay()
+
+    return {
+        monthName,
+        daysOfMonth,
+        startsOn
+    }
+    
+})
+
+const html = calendar.map(({daysOfMonth,monthName, startsOn}) => {
+    const days = [...Array(daysOfMonth).keys()] 
+
+    const firstDayAttributes = `class ='first-day' style='--first-day-start: ${startsOn}'`
+
+    const renderedDays = days.map((day, index) => `<li ${index == 0 ? firstDayAttributes : ''}>${day + 1}</li>`).join('')
+
+    const title = `<h1>${monthName} ${year}</h1>`
+    
+    return `${title}<ol>${renderedWeekDays} ${renderedDays}</ol>`
+}).join('')
+
+document.querySelector('div').innerHTML = html
+}
+
+createCalendar({year: 2023, locale: 'es'})
